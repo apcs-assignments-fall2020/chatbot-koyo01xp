a2ecc313695388a7c667f4a808aca961c45c8b63
koyo01xp <21kasakawa@athenian.org>
2020-09-02 05:44:45 +0000

    Chatbot code

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,10 +1,17 @@
 import java.util.Scanner;
-
 public class MyMain {
     public static void main(String[] args) {
+        // Write some code here!
         Scanner scan = new Scanner(System.in);
-
-
+        System.out.print("What is your name? ");
+        String name = scan.next();
+        System.out.println("Hello, " + name + "! It's nice to meet you!");
+        System.out.print("What is your favorite color, " + name + "? ");
+        String color = scan.next();
+        System.out.print("Oh I like " + color + " too!");
         scan.close();
+
+
+
     }
 }
